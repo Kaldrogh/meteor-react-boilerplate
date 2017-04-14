0340e84dadaaed0d14c4f95f66794a1f5ce53bc9
Kodos <kodos.kaldrogh@gmail.com>
2017-04-14 08:17:55 +0200

    Formatting some files

--- imports/ui/SearchLink.js
+++ imports/ui/SearchLink.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import TextField from 'material-ui/TextField';
+import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import TextField from "material-ui/TextField";
 
 const muiTheme = getMuiTheme({
   palette: {
-      primary1Color: '#ef6c00'
+    primary1Color: "#ef6c00"
   }
-})
+});
 
-const SearchLink = (props) => (
+const SearchLink = props => (
   <MuiThemeProvider muiTheme={muiTheme}>
-    <TextField onChange={props.onChange} value={props.inputValue} floatingLabelText="Search a Link" name="searchLink" fullWidth={true} onBlur={props.cleanInput}/>
+    <TextField
+      onChange={props.onChange}
+      value={props.inputValue}
+      floatingLabelText="Search a Link"
+      name="searchLink"
+      fullWidth={true}
+      onBlur={props.cleanInput}
+    />
   </MuiThemeProvider>
 );
 
